bf60f6d93c5915abc1b03f094ec4a9acd1d70751
Philipp Kolloczek <philipp.kolloczek@myrcon.com>
2012-07-30 15:18:06 +0200

    revoked thread.sleep replacement

--- src/PRoCon.Core/PRoConApplication.cs
+++ src/PRoCon.Core/PRoConApplication.cs
@@ -1784,21 +1784,7 @@ namespace PRoCon.Core {
         private void ReconnectVersionChecker() {
 
             while (this.m_isCheckerRunning == true) {
-                //Thread.Sleep(20000);
-                // taken from msdn social
-                Stopwatch sw = new Stopwatch();
-                sw.Start();
-                for (int i = 0; ; i++) {
-                    if (i % 100000 == 0) {
-                        sw.Stop();
-                        if (sw.ElapsedMilliseconds > 20000) {
-                            break;
-                        }
-                        else {
-                            sw.Start();
-                        }
-                    }
-                }
+                Thread.Sleep(20000);
 
                 // Send a report naow, next in 30 mins.
                 if (this.m_blInitialUsageDataSent == false && this.OptionsSettings.AllowAnonymousUsageData == true) {
