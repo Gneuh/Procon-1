f7c77ccb6f4a1e29605df0478c0551155126d113
maik.schoeneich <macgyverx83@gmail.com>
2013-10-27 19:13:50 +0100

    With the new VersionInfo.cs file you are able to manage Procon's Version Number in a single place. Procon Updater is out of scope, because it's version number already is out of sync to Procon

--- src/PRoCon.Core/Properties/AssemblyInfo.cs
+++ src/PRoCon.Core/Properties/AssemblyInfo.cs
@@ -8,11 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Procon.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("myrcon.com")]
-[assembly: AssemblyProduct("Procon.Core")]
-[assembly: AssemblyCopyright("Copyright © Myrcon Pty. Ltd. 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
 // für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
@@ -21,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // Die folgende Guid bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("a91d209b-6bf7-4656-962a-09f3ca77d31a")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.1.0")]
-[assembly: AssemblyFileVersion("1.4.1.0")]
--- src/PRoCon/Properties/AssemblyInfo.cs
+++ src/PRoCon/Properties/AssemblyInfo.cs
@@ -8,11 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Procon Frostbite")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("myrcon.com")]
-[assembly: AssemblyProduct("Procon Frostbite")]
-[assembly: AssemblyCopyright("Copyright © Myrcon Pty. Ltd. 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,15 +18,4 @@ using System.Runtime.InteropServices;
 // The following Guid is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4d939e34-b116-4c3a-a158-65e07d1d7dc5")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.1.0")]
-[assembly: AssemblyFileVersion("1.4.1.0")]
+
