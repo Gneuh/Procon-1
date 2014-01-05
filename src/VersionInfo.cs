9b6316e18148791426ce291745cacc7e60355a1e
MorpheusXAUT <nick.mueller@myrcon.com>
2014-01-05 13:29:14 +0100

    Updated version number to 1.4.1.4 Updated copyright year

--- src/VersionInfo.cs
+++ src/VersionInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Myrcon Pty. Ltd.")]
 [assembly: AssemblyProduct("Procon Frostbite")]
-[assembly: AssemblyCopyright("Copyright © Myrcon Pty. Ltd. 2013")]
+[assembly: AssemblyCopyright("Copyright © Myrcon Pty. Ltd. 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.1.3")]
-[assembly: AssemblyFileVersion("1.4.1.3")]
+[assembly: AssemblyVersion("1.4.1.4")]
+[assembly: AssemblyFileVersion("1.4.1.4")]
